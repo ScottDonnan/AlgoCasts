47ad8cf54ccbb238f462320f20286d845af5f106
Scott Donnan <scott.donnan@me.com>
2022-02-07 16:32:13 -0600

    solution added

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,16 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const counter = new Object
+    let largest = str[0];
+    for (const value of str) {
+        counter[value] = counter[value] + 1 || 1
+        if (counter[value] > counter[largest]) {
+            largest = value
+        }
+    }
+    return largest
+}
 
 module.exports = maxChar;
