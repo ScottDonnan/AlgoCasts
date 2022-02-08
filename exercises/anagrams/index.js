46c8b3cbf01b4abfb892bb10fed64e5266a7e19f
Scott Donnan <scott.donnan@me.com>
2022-02-08 14:35:51 -0600

    solutions

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,78 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    let anagram = true;
+    const aObj = createCounter(stringA)
+    const bObj = createCounter(stringB)
+  
+    function createCounter(string) {
+      const charMap = {}
+      const regString = string.replace(/[^\w]/g, "").toLowerCase()
+
+      for (let elem of regString) {
+        charMap[elem] ? charMap[elem]++ : charMap[elem] = 1
+      }
+
+      return charMap
+    }
+  
+
+    if (Object.keys(aObj).length > Object.keys(bObj).length) {
+      for (let key in aObj) {
+        if (aObj[key] !== bObj[key]) {
+            anagram = false
+          }
+      }
+    } else {
+      for (let key in bObj) {
+        if (aObj[key] !== bObj[key]) {
+            anagram = false
+          }
+      }
+    }
+
+    return anagram
+
+}
 
 module.exports = anagrams;
+
+
+// function anagrams(stringA, stringB) {
+//     const aObj = {}
+//     const bObj = {}
+  
+//     const regStringA = stringA.replace(/[^\w]/g, "").toLowerCase()
+//     const regStringB = stringB.replace(/[^\w]/g, "").toLowerCase()
+//     let anagram = true;
+    
+//     for (let elem of regStringA) {
+//         aObj[elem] ? aObj[elem]++ : aObj[elem] = 1
+//     }
+
+//     for (let elem of regStringB) {
+//         if (bObj[elem]) {
+//             bObj[elem]++
+//         } else {
+//             bObj[elem] = 1
+//         }
+//     }
+
+//     if (regStringA.length > regStringB) {
+//       for (let key in aObj) {
+//         if (aObj[key] !== bObj[key]) {
+//             anagram = false
+//           }
+//       }
+//     } else {
+//       for (let key in bObj) {
+//         if (aObj[key] !== bObj[key]) {
+//             anagram = false
+//           }
+//       }
+//     }
+
+//     return (anagram)
+
+// }
